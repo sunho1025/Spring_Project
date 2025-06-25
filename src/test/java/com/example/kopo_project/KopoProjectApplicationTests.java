d1cee3a3188b6f690bd4ef8b8f4961e679e8151c
sunho1025 <sdu10722@gmail.com>
2025-06-25 16:57:06 +0900

    no message

---
+++ src/test/java/com/example/kopo_project/KopoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kopo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
